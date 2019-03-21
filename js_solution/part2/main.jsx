a89d9b5a949788c5083961ae5bcb7cc3fd047e63
jcding <jcding@umich.edu>
2019-03-21 00:55:50 -0400

    removed js_solution

--- js_solution/part2/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RandomPannel from './randompannel.jsx';
-
-ReactDOM.render(
-  <RandomPannel url="/api/v1/message/" />,
-  document.getElementById('randomWidgetEntry'),
-);
